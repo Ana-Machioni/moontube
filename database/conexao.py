c03c7dd0c160d91c593209c33886e2c808b02f70
Ana Laura <ana.machioni@aluno.senai.br>
2026-03-27 16:09:16 -0300

    add: requirements aqui gunicorn

--- database/conexao.py
+++ database/conexao.py
@@ -1,21 +1,32 @@
 import mysql.connector
 
-def conectar(): 
+def conectar():
+    tipo_conexao = "Nuvem" 
+    if tipo_conexao == "local":
+        #Conectando no bd
+        conexao = mysql.connector.connect(
+            host="localhost",
+            port=3306,
+            user="root",
+            password="root",
+            database="Musica"
+        )
 
-    #Conectando no bd
-    conexao = mysql.connector.connect(
-        host="localhost",
-        port=3306,
-        user="root",
-        password="root",
-        database="Musica"
-    )
+    else:
+        conexao = mysql.connector.connect(
+            host="servidor-anadz-servidor-anadz.a.aivencloud.com",
+            port=28799,
+            user="avnadmin",
+            password="AVNS_2l6qHVA-QZ89CJ-75rp",
+            database="Musica"
+        )
 
-        #Criando o cursor
-    cursor = conexao.cursor(dictionary=True)
-        
 
-    return conexao, cursor 
+            #Criando o cursor
+        cursor = conexao.cursor(dictionary=True)
+            
+
+        return conexao, cursor 
 
 
 
